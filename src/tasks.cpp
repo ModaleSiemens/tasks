5a42d5f8ec70c69cbbb30c25557fa1d5891a041d
ModaleSiemens <simonedemelas05@gmail.com>
2024-09-21 11:19:24 +0200

    Write delta time game loop

--- src/tasks.cpp
+++ src/tasks.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include "app.hpp"
+
+class TasksApp : public app::Application 
+{
+    public:
+
+};
 
 int main()
 {
+    using namespace std::chrono_literals;
+    using namespace std::chrono;
+
+    TasksApp app;
+
+    steady_clock::time_point new_timepoint {steady_clock::now()};
+    steady_clock::time_point old_timepoint;
+
+    while(true)
+    {
+        old_timepoint = new_timepoint;
+        new_timepoint = steady_clock::now();
+
+        app.update(new_timepoint - old_timepoint);
+    }
 
     return 0;
-}
+} 
